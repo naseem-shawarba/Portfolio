3e95ce5d17bae6c840fb1c5b7b16b3f99638064f
naseem-shawarba <naseem.shawarba@gmail.com>
2024-05-27 18:32:24 +0200

    created empty vue vuetify nuxt project

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+	<v-layout>
+		<v-main>
+			<v-container fluid>
+				<slot />
+			</v-container>
+		</v-main>
+		<div>
+			<v-footer :absolute="false" app>
+				<span>&copy; {{ new Date().getFullYear() }}</span>
+			</v-footer>
+		</div>
+
+	</v-layout>
+</template>
