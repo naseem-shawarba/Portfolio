80ba47135669a8592f4c99024fd9d6a46d5b103a
naseem-shawarba <naseem.shawarba@gmail.com>
2024-05-29 18:34:52 +0200

    Added content to the footer file, including links to important pages and social media icons.

--- components/Layout/LayoutFooter.vue
+++ components/Layout/LayoutFooter.vue
@@ -1,7 +1,35 @@
 <template>
 	<div>
 		<v-footer :absolute="false" app>
-			<span>&copy; {{ new Date().getFullYear() }}</span>
+			<!-- <div style="width:100%" >
+				<v-btn v-for="account in accounts" :key="account.title" :icon="account.icon" :href="account.link"
+					target="_blank" class="mx-4" color="white" />
+			</div>
+			<div style="border: 2px solid black;  width: 100%;" class="text-center">Copyright &copy; {{ new Date().getFullYear() }} Naseem Shawarba
+			</div> -->
+			<v-card width="100%" class="text-center" :elevation="0">
+				<v-card-actions class="d-flex justify-center">
+						<v-btn v-for="account in accounts" :key="account.title" :icon="account.icon" :href="account.link"
+						target="_blank" class="mx-2" />
+				</v-card-actions>
+				<v-card-text>
+					Copyright &copy; {{ new Date().getFullYear() }} Naseem Shawarba
+				</v-card-text>
+			</v-card>
+
 		</v-footer>
 	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			accounts: [
+				{ title: "linkedin", icon: "mdi-linkedin", link: "https://www.linkedin.com/in/naseem-shawarba/" },
+				{ title: "github", icon: "mdi-github", link: "https://github.com/naseem-shawarba" }
+			]
+		}
+	}
+}
+</script>
