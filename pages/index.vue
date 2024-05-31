49ad1733793b2971832a268152b42e4d21db6053
naseem-shawarba <naseem.shawarba@gmail.com>
2024-05-31 21:54:04 +0200

    Made sections addressable for better navigation and URL linking

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
 		<h1>Website Still Under Construction...</h1>
-		<AboutMeSection />
-		<ProjectsSection />
-		<ContactSection />
+		<AboutMeSection id="about-me"/>
+		<ProjectsSection id="projects"/>
+		<ContactSection id="contact"/>
 	</div>
 
 </template>
