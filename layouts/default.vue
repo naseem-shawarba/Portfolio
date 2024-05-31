579d71c36f3fa01450eaca7bc5fc0bf01e386028
naseem-shawarba <naseem.shawarba@gmail.com>
2024-05-31 09:46:32 +0200

    make v-container width responsive to screen size

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,11 @@
 <template>
 	<v-layout>
 		<v-main>
-			<v-container style="width:80%" fluid>
+			<v-container :style="{ width: $vuetify.display.smAndDown ? '100%' : '80%'}" fluid>
 				<slot />
 			</v-container>
 		</v-main>
+
 
 		<LayoutFooter />
 	</v-layout>
