4eddec675df2db431b006ad4cedacd011d68f6d4
naseem-shawarba <naseem.shawarba@gmail.com>
2024-06-01 22:43:27 +0200

    Removed contact information from footer

--- components/Layout/LayoutFooter.vue
+++ components/Layout/LayoutFooter.vue
@@ -8,10 +8,6 @@
 			<div style="border: 2px solid black;  width: 100%;" class="text-center">Copyright &copy; {{ new Date().getFullYear() }} Naseem Shawarba
 			</div> -->
 			<v-card width="100%" class="text-center" :elevation="0">
-				<v-card-actions class="d-flex justify-center">
-						<v-btn v-for="account in accounts" :key="account.title" :icon="account.icon" :href="account.link"
-						target="_blank" class="mx-2" />
-				</v-card-actions>
 				<v-card-text>
 					Copyright &copy; {{ new Date().getFullYear() }} Naseem Shawarba
 				</v-card-text>
@@ -20,16 +16,3 @@
 		</v-footer>
 	</div>
 </template>
-
-<script>
-export default {
-	data() {
-		return {
-			accounts: [
-				{ title: "linkedin", icon: "mdi-linkedin", link: "https://www.linkedin.com/in/naseem-shawarba/" },
-				{ title: "github", icon: "mdi-github", link: "https://github.com/naseem-shawarba" }
-			]
-		}
-	}
-}
-</script>
