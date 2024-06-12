52bc0ec188e825978374b6cdea169c23f1143e26
naseem-shawarba <naseem.shawarba@gmail.com>
2024-06-12 20:53:30 +0200

    Positioned buttons at the end of the last row in project chip list

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<v-hover v-slot="{ isHovering, props }" close-delay="150">
-			<v-card style="border-radius:20px;" :class="['ma-3', { 'on-hover': isHovering }]"
+			<v-card style="border-radius: 20px" :class="['ma-3', { 'on-hover': isHovering }]"
 				:elevation="isHovering ? 16 : 2" v-bind="props">
 				<v-img class="mt-0 zoom-cursor" @click="dialog = !dialog" :src="project.screenshot"></v-img>
 
@@ -12,20 +12,19 @@
 				<v-card-subtitle class="pb-0 text-wrap mb-2">
 					{{ project.description }}
 				</v-card-subtitle>
-				<div class="d-flex mb-1">
-					<span>
-						<v-chip v-for="technologie in project.technologies" :key="technologie" outlined
-							class="ml-2 mb-2">
-							{{ technologie }}
-						</v-chip>
+				<div class="d-flex flex-wrap mb-1">
+					<v-chip v-for="technologie in project.technologies" :key="technologie" outlined class="ml-2 mb-2">
+						{{ technologie }}
+					</v-chip>
+
+					<span class="ml-auto">
+						<v-btn v-if="project.deployedWebsite" class=" ml-4 mr-2" flat density="compact"
+							icon="mdi-open-in-new" :href="project.deployedWebsite" target="_blank"></v-btn>
+						<v-btn v-if="project.DeployedAndroidApp" class="align-self-end mr-1" flat density="compact"
+							icon="mdi-google-play" :href="project.DeployedAndroidApp" target="_blank"></v-btn>
+						<v-btn v-if="project.githubLink" class="align-self-end mr-1" flat density="compact"
+							icon="mdi-github" :href="project.githubLink" target="_blank"></v-btn>
 					</span>
-					<v-spacer></v-spacer>
-					<v-btn v-if="project.deployedWebsite" class="align-self-end mb-1 mr-2" flat density="compact"
-						icon="mdi-open-in-new" :href="project.deployedWebsite" target="_blank"></v-btn>
-					<v-btn v-if="project.DeployedAndroidApp" class="align-self-end mb-1 mr-1" flat density="compact"
-						icon="mdi-google-play" :href="project.DeployedAndroidApp" target="_blank"></v-btn>
-					<v-btn v-if="project.githubLink" class="align-self-end mb-1 mr-1" flat density="compact"
-						icon="mdi-github" :href="project.githubLink" target="_blank"></v-btn>
 				</div>
 			</v-card>
 		</v-hover>
@@ -40,7 +39,7 @@
 <script>
 export default {
 	props: {
-		project: Object
+		project: Object,
 	},
 	data() {
 		return {
@@ -60,11 +59,12 @@ export default {
 	height: 100%;
 	object-fit: contain;
 }
-.v-card{
+
+.v-card {
 	transition: transform 0.3s ease;
 }
-.v-card:hover{
+
+.v-card:hover {
 	transform: scale(1.02);
 }
-
 </style>
