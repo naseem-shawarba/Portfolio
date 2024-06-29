71c079aa69200b711ee0f834b30b53f5d6780cd1
naseem-shawarba <naseem.shawarba@gmail.com>
2024-06-29 15:46:29 +0200

    Added a hero section

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 	<div>
 		<v-alert v-show="false" id="message" class="mb-2" color="orange" icon="mdi-hammer" theme="dark"
 			title="Website Still Under Construction..." closable />
-		yo
+		<HeroSection />
 		<div :style="{ width: $vuetify.display.smAndDown ? '100%' : '80%', margin: '0 auto'}" fluid>
 			<AboutMeSection v-show="false" />
 			<ProjectsSection />
---
+++ plugins/vue-writer.js
@@ -0,0 +1,6 @@
+import VueWriter from "vue-writer";
+
+// Vue.use(VueWriter);
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueWriter);
+});
