5d693216d273654e0f49bb726cc05757f867d9e8
naseem-shawarba <naseem.shawarba@gmail.com>
2024-06-29 16:06:07 +0200

    Added a scroll to top button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,47 @@
 <template>
-	<v-layout>
-		<v-main>
-			<v-container fluid>
-				<slot />
-			</v-container>
-		</v-main>
-		<LayoutFooter />
-	</v-layout>
+  <v-layout>
+    <v-main>
+      <v-btn
+        v-show="showGoToTop"
+        class="mr-5 mb-8 position-fixed right-0 bottom-0"
+        style="z-index: 1000"
+        color="green"
+        icon="mdi-chevron-up"
+        v-scroll="onScroll"
+        @click="scrollToTop"
+      ></v-btn>
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+    <LayoutFooter />
+  </v-layout>
 </template>
+
+<script>
+import { useGoTo } from "vuetify";
+export default {
+  setup() {
+    const goTo = useGoTo();
+    return { goTo };
+  },
+  data() {
+    return {
+      offsetTop: 0
+    };
+  },
+  computed: {
+    showGoToTop() {
+      return this.offsetTop > 200;
+    },
+  },
+  methods: {
+    onScroll(event) {
+      this.offsetTop = event.target.scrollingElement.scrollTop;
+    },
+    scrollToTop() {
+      this.goTo(0);
+    },
+  },
+};
+</script>
