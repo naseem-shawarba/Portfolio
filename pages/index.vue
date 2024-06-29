4ddc56f031f125e626b7be66906525b11d7a8fde
naseem-shawarba <naseem.shawarba@gmail.com>
2024-06-29 14:52:09 +0200

    Hide 'Website Still Under Construction' message from homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<v-alert id="message" class="mb-2" color="orange" icon="mdi-hammer" theme="dark"
+		<v-alert v-show="false" id="message" class="mb-2" color="orange" icon="mdi-hammer" theme="dark"
 			title="Website Still Under Construction..." closable />
 		<AboutMeSection v-show="false" id="about-me"/>
 		<ProjectsSection id="projects"/>
